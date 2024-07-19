ed26a6b62440defa7890e4dff96492938f44d2a1
KingCrusher275 <mmaiti627@gmail.com>
2024-07-19 00:13:26 -0700

    Added ssh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
 import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './App.css'
 
 
+function SshCreds({ setIsSsh }) {
+
+  async function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/connect", {
+        username: formData.get('username'),
+        host: formData.get('host'),
+        password: formData.get('password')
+      });
+      if (response.status === 200)
+        setIsSsh(true);
+      else
+        setIsSsh(false);
+    } catch (error) {
+      console.error('Error connecting:', error);
+      setIsSsh(false);
+    }
+  }
+
+  return (
+    <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onFormSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="host" type="host" placeholder="Host" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            Username
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
 function Form({ runCommand }) {
   function onFormSubmit(event) {
 
@@ -61,34 +113,37 @@ function Form({ runCommand }) {
 function CommandInterface() {
   const [command, setCommand] = useState('');
   const [output, setOutput] = useState('');
+  const [isSsh, setIsSsh] = useState(false);
   const outputRef = useRef(null);
   const wsRef = useRef(null);
 
   useEffect(() => {
-    wsRef.current = new WebSocket('ws://localhost:3000');
+    if (isSsh) {
+      wsRef.current = new WebSocket('ws://127.0.0.1:5000');
 
-    wsRef.current.onopen = () => {
-      console.log('WebSocket connection established');
-    };
+      wsRef.current.onopen = () => {
+        console.log('WebSocket connection established');
+      };
 
-    wsRef.current.onmessage = (event) => {
-      setOutput(prevOutput => prevOutput + event.data + '\n');
-    };
+      wsRef.current.onmessage = (event) => {
+        setOutput(prevOutput => prevOutput + event.data + '\n');
+      };
 
-    wsRef.current.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
+      wsRef.current.onerror = (error) => {
+        console.error('WebSocket error:', error);
+      };
 
-    wsRef.current.onclose = () => {
-      console.log('WebSocket connection closed');
-    };
+      wsRef.current.onclose = () => {
+        console.log('WebSocket connection closed');
+      };
 
-    return () => {
-      if (wsRef.current) {
-        wsRef.current.close();
-      }
-    };
-  }, []);
+      return () => {
+        if (wsRef.current) {
+          wsRef.current.close();
+        }
+      };
+    }
+  }, [isSsh]);
 
   const runCommand = (cmd) => {
     if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
@@ -104,13 +159,14 @@ function CommandInterface() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="font-sans text-3xl m-5">
-        Configure Devices
-      </h1>
-      <Form runCommand={runCommand} />
-      <CustomCommand command={command} setCommand={setCommand} runCommand={runCommand} output={output} outputRef={outputRef} />
+      <h1 className="font-sans text-3xl m-5">Configure Devices</h1>
+      <div className="flex items-center justify-around w-full">
+        <SshCreds setIsSsh={setIsSsh} />
+        {isSsh && <Form runCommand={runCommand} />}
+      </div>
+      {isSsh && <CustomCommand command={command} setCommand={setCommand} runCommand={runCommand} output={output} outputRef={outputRef} />}
     </div>
-  )
+  );
 }
 
 function CustomCommand({ command, setCommand, runCommand, output, outputRef }) {
@@ -129,7 +185,7 @@ function CustomCommand({ command, setCommand, runCommand, output, outputRef }) {
       <h1>Command Output</h1>
       <input
         type="text"
-        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         value={command}
         onChange={(e) => setCommand(e.target.value)}
         onKeyPress={handleKeyPress}
